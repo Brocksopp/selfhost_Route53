06d8c491b35df93254c070c547f0b73df86c25fc
Brock <Brocksopp@users.noreply.github.com>
2019-05-16 23:15:44 +0100

    SupportMultipleSubDomain
    -Now supports multiple sub-domains, controlled via the config file.
    -Broke down large functions into smaller ones.

--- selfhost_Route53/selfhost_Route53.py
+++ selfhost_Route53/selfhost_Route53.py
@@ -13,9 +13,21 @@ class update_ip:
     def __init__(self):
          self.aws_access_key_id = None
          self.aws_secret_access_key = None
-         self.domain = None
+         self.hostZone = None
+         #self.domain = None
+         self.subDomain = None
          self.currentIP = None
+         self.conn = None ##Variable for Amazon connection
          pass
+    
+
+    def main(self):
+        self.get_config()
+        self.get_PublicIP()
+        self.get_connection()
+        self.get_DomainDetails()
+        self.update_IP()
+        pass
 
 
     def get_config(self):
@@ -25,15 +37,19 @@ class update_ip:
                 data = json.load(json_file)
                 self.aws_access_key_id = data["aws_access_key_id"]
                 self.aws_secret_access_key = data["aws_secret_access_key"]
-                self.domain = data["domain"]
+                self.hostedZone = data["hostedZone"]
+                #self.domain = data["domain"]
+                self.subDomain = data["subDomain"]
                 pass
             pass
         else:
             print("Config File Not Found.")
             return 1
+        pass
 
 
-    def get_IP(self):
+
+    def get_PublicIP(self):
         req = urllib.request.Request('https://api.ipify.org') ##Get IP from web API.
         with urllib.request.urlopen(req) as response:
             self.currentIP = response.read().decode('utf-8')
@@ -41,9 +57,33 @@ class update_ip:
         print("Current IP = " + self.currentIP)
         return self.currentIP
 
-    def update_IP(self):
 
-        if (self.domain == None or self.domain == ''):
+    def get_connection(self):
+
+        print("Connecting to AWS Route53...")
+        self.conn = route53.connect(
+            aws_access_key_id = self.aws_access_key_id,
+            aws_secret_access_key = self.aws_secret_access_key,)
+
+        ## Verify connection was successful??
+
+        pass
+
+
+    def get_DomainDetails(self):
+        for zone in self.conn.list_hosted_zones():
+            print("Hosted Zone: " + zone.name)
+            for record_set in zone.record_sets:
+                print("\t" + record_set.name + ", Type: " + record_set.rrset_type)
+                for records in record_set.records:
+                    print("\t\t" + records)
+                pass
+            pass
+        pass
+
+
+    def update_IP(self):
+        if (self.subDomain == None or self.subDomain == []):
             print("Settings Incomplete.")
             return 1
 
@@ -51,45 +91,30 @@ class update_ip:
             print("Settings Incomplete.")
             return 1
 
-        print("Connecting to AWS Route53...")
-    
-        conn = route53.connect(
-            aws_access_key_id = self.aws_access_key_id,
-            aws_secret_access_key = self.aws_secret_access_key,)
-        
-
-
-        for zone in conn.list_hosted_zones():
-            print(zone.name)
-            for record_set in zone.record_sets:
-                print("\t" + record_set.name)
-                print("\t\t" + record_set.zone_id)
-                pass
-            pass
-
-        print("...")
         # Note that this is a fully-qualified domain name.
-        name_to_match = self.domain
-        for record_set in zone.record_sets:
-            if record_set.name == name_to_match:
-                print(record_set)
-                # Stopping early may save some additional HTTP requests,
-                # # since zone.record_sets is a generator.
-                break
-            pass
+        for zone in self.conn.list_hosted_zones():
+            if zone.name == (self.hostedZone + "."):
+                for record_set in zone.record_sets:
+                    for prefix in self.subDomain:
+                        address = (prefix + "." + self.hostedZone)
+                        if record_set.name == (address + "."):
+                            #print(record_set.name)
+                            # Stopping early may save some additional HTTP requests,
+                            # # since zone.record_sets is a generator.
 
-        ##Check whether existing IP matches current IP.
-        if record_set.records == [self.currentIP]:
-            print("No Change Required.")
-        else:
-            #Changing record set
-            ##record_set.records = ['8.8.8.7']
-            record_set.records = [self.currentIP]
-            record_set.save()
-            print("IP Updated.")
-            pass
+                            ##Check whether existing IP matches current IP.
+                            if record_set.records == [self.currentIP]:
+                                print("No Change Required: " + address)
+                            else:
+                                #Changing record set
+                                ###record_set.records = ['8.8.8.7']
+                                record_set.records = [self.currentIP]
+                                record_set.save()
+                                print("IP Updated: " + address)
+                                pass
+                            break
+                    pass
         pass
-
 
     pass ##End of Class
 
@@ -100,7 +125,5 @@ class update_ip:
 
 
 instance = update_ip()
-instance.get_config()
-instance.get_IP()
-instance.update_IP()
+instance.main()
 pass
